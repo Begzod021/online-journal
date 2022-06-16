57758eb78e2203c821e40621ddf25cf6f281dd17
Begzod Rasulev <begzod_rasulev@mail.ru>
2022-06-16 19:58:51 +0500

    first project

--- config/frontend/src/App.js
+++ config/frontend/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios'; // new
+const list = [
+    {
+        "id": 13,
+        "class_number": {
+            "class_num": "10"
+        },
+        "subject": {
+            "id": 1,
+            "subjects": "Ingliz Tili",
+            "teacher": 26
+        },
+        "ratings": 5,
+        "teacher": 26,
+        "pupil": {
+            "username": "Begzod",
+            "role": "pupil"
+        }
+    },
+    {
+        "id": 14,
+        "class_number": {
+            "class_num": "10"
+        },
+        "subject": {
+            "id": 2,
+            "subjects": "Ona Tili",
+            "teacher": 26
+        },
+        "ratings": 4,
+        "teacher": 26,
+        "pupil": {
+            "username": "Begzod",
+            "role": "pupil"
+        }
+    }
+]
+class App extends Component {
+    constructor(props) {
+    super(props);
+    this.state = { list };
 }
-
+    render() {
+    return (
+    <div>
+    {this.state.list.map(item => (
+    <div key={item.id}>
+    <h1>{item.subject}</h1>
+    <p>{item.ratings}</p>
+    </div>
+    ))}
+    </div>
+    );
+    }
+}
 export default App;
